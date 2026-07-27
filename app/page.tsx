130a5b3fda83f75eb90907a2a7617b8c5ecca045
Furkan Kesoğlu <118119571+furkankesoglu@users.noreply.github.com>
2026-07-27 21:42:28 +0300

    Show latest check-in weight in header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import DeleteRecordUx from "../src/components/DeleteRecordUx";
+import LiveWeightSync from "../src/components/LiveWeightSync";
 import MikeCoachSystem from "../src/components/MikeCoachSystem";
 
 export default function Home() {
   return (
     <>
       <DeleteRecordUx />
+      <LiveWeightSync />
       <MikeCoachSystem />
     </>
   );
