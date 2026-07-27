33dbdc339fe83a498f74fcdfaf574ca5879b2f3e
Furkan Kesoğlu <118119571+furkankesoglu@users.noreply.github.com>
2026-07-27 15:03:02 +0300

    Add MIKE v4 route to active root app

---
+++ app/v4/page.tsx
@@ -0,0 +1,8 @@
+import MikeCoachSystem from "../../src/components/MikeCoachSystem";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function V4Page() {
+  return <MikeCoachSystem />;
+}
