eb0b7c750036e29f13023f1b8b893e29dc18554f
Furkan Kesoğlu <118119571+furkankesoglu@users.noreply.github.com>
2026-07-27 15:37:09 +0300

    Use single authoritative cloud status

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import CloudStatus from "../src/components/CloudStatus";
 import MikeCoachSystem from "../src/components/MikeCoachSystem";
 
 export default function Home() {
-  return (
-    <>
-      <CloudStatus />
-      <MikeCoachSystem />
-    </>
-  );
+  return <MikeCoachSystem />;
 }
